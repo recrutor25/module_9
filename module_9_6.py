90da37db9007f0850f58ac3b6d4aabe0aad6642a
recrutor25 <sommersi@yandex.ru>
2025-01-09 23:32:33 +0200

    09.01.25_2

--- module_9_6.py
+++ module_9_6.py
@@ -1,2 +1,12 @@
 #!/home/igor/PycharmProjects/module_9/.venv/bin/python
 # -*- coding: utf-8 -*-
+
+def all_variants(text):
+   for i in range(1, len(text) + 1):
+        for j in range(len(text) +1 - i):
+            yield text[j:j + i]
+
+
+a = all_variants("abcd")
+for i in a:
+    print(i)
