98a37f87a847a6b907ff8907478d8a19d2ec578c
recrutor25 <sommersi@yandex.ru>
2025-01-11 14:22:16 +0200

    11.01.25_2

--- module_9_7.py
+++ module_9_7.py
@@ -1,25 +1,25 @@
 #!/home/igor/PycharmProjects/module_9/.venv/bin/python
 # -*- coding: utf-8 -*-
-
-
 def is_prime(func):
     def wrapper(*args):
-        res = func(*args)
-        for i in range(2, int(res ** 0.5 + 1)):
-            if res % i == 0:
-                print("Составное")
-                break
-        else:
-            print('Простое')
-        return res
+        summa = func(*args)
+        is_prime = True
+        if summa > 1:
+            for i in range(2, (summa//2)+1):
+                if summa % i == 0:
+                 is_prime = False
+            if is_prime == True:
+                return (f'Простое\n{summa}')
+
+            else:
+                return (f'Составное\n{summa}')
 
     return wrapper
 
-
 @is_prime
-def sum_3(*args: int):
+def sum_three(*args: int):
     return sum(args)
 
-
-result = sum_3(2, 3, 6)
+result = sum_three(2, 3, 6)
 print(result)
+
